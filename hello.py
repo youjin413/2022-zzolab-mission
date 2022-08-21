944089bb3080b34885afc6275abb0f84dbc069dc
youjin413 <111631885+youjin413@users.noreply.github.com>
2022-08-21 21:47:41 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
-# 저의 첫 커밋입니다!
-print('hello world')
+import random
+
+print('다음 구구단을 풀어주세요! 총 10문제가 제공됩니다.')
+
+score = 0
+
+for i in range(1,11) :
+  a = random.randint(1,9)
+  b = random.randint(1,9)
+  print(a, 'x',  b, '=')
+  x = input('답 : ')
+
+  if a * b == int(x) :
+    score = score + 1
+    print('정답입니다!')
+  else :
+    print('틀렸습니다!')
+
+print('당신의 점수는 ', score, '점 입니다.')
